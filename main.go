05439511fa48a50b7df4df715281da065ec94770
Paul Tobin <tobster111@gmail.com>
2026-02-13 16:30:06 +0000

    refactor main.go to enhance routing and add health check endpoint

--- main.go
+++ main.go
@@ -9,11 +9,18 @@ func main() {
 	const filepathRoot = "."
 	const port = "8080"
 
+	
 	mux := http.NewServeMux()
-	mux.Handle("/", http.FileServer(http.Dir(filepathRoot)))
+	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot))))
 	// configure the filepath so that the image is accessible from http://localhost:8080/assets/logo.png
 	mux.Handle("/assets/", http.StripPrefix("/assets/", http.FileServer(http.Dir(filepathRoot+"/assets"))))	
-
+	// The endpoint should be accessible at the /healthz path using any HTTP method.
+	
+	mux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("OK"))
+	})
 	srv := &http.Server{
 		Addr:    ":" + port,
 		Handler: mux,
