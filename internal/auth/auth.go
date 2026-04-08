7c6e22f7a7f35ed62d393861f036a36fe7544b65
Paul Tobin <tobster111@gmail.com>
2026-04-08 17:08:16 +0100

    feat: add MakeRefreshToken function for generating secure refresh tokens

--- internal/auth/auth.go
+++ internal/auth/auth.go
@@ -1,6 +1,8 @@
 package auth
 
 import (
+	"crypto/rand"
+	"encoding/hex"
 	"errors"
 	"fmt"
 	"net/http"
@@ -60,4 +62,13 @@ func GetBearerToken(headers http.Header) (string, error) {
 		return "", errors.New("authorization header malformed")
 	}
 	return strings.TrimSpace(authHeader[len("Bearer "):]), nil
+}
+
+func MakeRefreshToken() (string, error) {
+	b := make([]byte, 32)
+	_, err := rand.Read(b)
+	if err != nil {
+		return "", err
+	}
+	return hex.EncodeToString(b), nil
 }
