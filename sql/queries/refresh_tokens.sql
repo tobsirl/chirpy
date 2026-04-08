99b431c30b31895b978dabe56eb5962a4644260d
Paul Tobin <tobster111@gmail.com>
2026-04-08 17:12:11 +0100

    feat: add RevokeRefreshToken functionality to update token status in the database

--- sql/queries/refresh_tokens.sql
+++ sql/queries/refresh_tokens.sql
@@ -16,3 +16,8 @@ INNER JOIN refresh_tokens ON users.id = refresh_tokens.user_id
 WHERE refresh_tokens.token = $1
 AND refresh_tokens.expires_at > NOW()
 AND refresh_tokens.revoked_at IS NULL;
+
+-- name: RevokeRefreshToken :exec
+UPDATE refresh_tokens
+SET revoked_at = NOW(), updated_at = NOW()
+WHERE token = $1;
